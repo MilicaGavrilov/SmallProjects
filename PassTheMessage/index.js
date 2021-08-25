9608ccd6b74ce8d856d45989f95c03c965d725e4
MilicaGavrilov <87259110+MilicaGavrilov@users.noreply.github.com>
2021-08-25 20:20:16 +0300

    Add files via upload

---
+++ PassTheMessage/index.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("#message-form");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const h5 = document.querySelector("h5");
+    const userInput = document.querySelector("#message");
+    const messageContent = document.querySelector(".message-content");
+
+    if (userInput.value === '') {
+        h5.classList.add("show");
+        setTimeout(function() {
+            h5.classList.remove("show");
+        }, 3000)
+    }else{
+        messageContent.textContent = userInput.value;
+    }
+    userInput.value = "";
+});
